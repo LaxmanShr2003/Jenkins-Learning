00ee32409e9c90438271ad0d39c4c428f85e280c
Laxman <laxmangit9861@gmail.com>
2026-01-05 03:09:01 +0000

    added groovy file

---
+++ project1/deploy.groovy
@@ -0,0 +1,55 @@
+pipeline {
+    agent any
+
+       
+    parameters {
+        string(
+            name: 'SERVER_IP',
+            defaultValue: '100.145.135.33',
+            description: 'Enter server IP address'
+            )
+    }
+
+    environment {
+        SSH_KEY64 = credentials('SSH_KEY64') 
+    }
+
+    stages {
+
+        stage('Configure SSH') {
+            steps {
+                sh '''
+                mkdir -p ~/.ssh
+                chmod 700 ~/.ssh
+
+                echo -e "Host *\\n\\tStrictHostKeyChecking no\\n" > ~/.ssh/config
+                chmod 600 ~/.ssh/config
+
+                touch ~/.ssh/known_hosts
+                chmod 600 ~/.ssh/known_hosts
+
+                cat ~/.ssh/config
+                '''
+            }
+        }
+
+        stage('Prepare SSH Key') {
+            steps {
+                sh '''
+                echo "$SSH_KEY64" | base64 -d > mykey.pem
+                chmod 600 mykey.pem
+                ssh-keygen -R ${SERVER_IP} || true
+                '''
+            }
+        }
+
+        stage('Deploy Code to Server') {
+            steps {
+                sh '''
+                ssh -i mykey.pem ubuntu@${SERVER_IP} \
+                "cd /var/www/html && git pull origin main"
+                '''
+            }
+        }
+    }
+}
